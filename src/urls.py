3b5867dd4cac69a42bec4494a14d2d285dc2de8c
root <root@thaifurnitureonline.com>
2013-07-18 02:28:48 +0000

    Major restructure, templates added into project finally.

--- src/urls.py
+++ src/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls.defaults import *
 
 from satchmo_store.urls import urlpatterns
 
+#urlpatterns += patterns('',
+#    (r'^favicon.ico$', 'django.views.generic.simple.redirect_to', {'url': '%sfavicon.ico' % settings.STATIC_URL}),
+#)
+
 urlpatterns += patterns('django.contrib.flatpages.views',
     url(r'^about/$', 'flatpage', {'url': '/about/'}, name='about'),
     #url(r'^paysbuyresp/$', 'payment.modules.paysbuy.views.tester'),
