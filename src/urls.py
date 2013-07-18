a090b9ad0eea38acc50cdf3830b39a045e585c96
root <root@thaifurnitureonline.com>
2013-07-18 05:38:04 +0000

    fixing layout #4 #5

--- src/urls.py
+++ src/urls.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls.static import static
 from django.conf.urls.defaults import *
-
+from django.conf import settings
 from satchmo_store.urls import urlpatterns
 
-#urlpatterns += patterns('',
-#    (r'^favicon.ico$', 'django.views.generic.simple.redirect_to', {'url': '%sfavicon.ico' % settings.STATIC_URL}),
-#)
+urlpatterns += patterns('',
+    (r'^favicon.ico$', 'django.views.generic.simple.redirect_to', {'url': '%sfavicon.ico' % settings.STATIC_URL}),
+)
 
 urlpatterns += patterns('django.contrib.flatpages.views',
     url(r'^about/$', 'flatpage', {'url': '/about/'}, name='about'),
